684d2d5c691ae0ffc8dbb78c6aac80d14db4c4cc
Inbestigator <inbestigatoryt@gmail.com>
2025-04-12 04:04:51 -0700

    Don't use fastify layer

--- api/bot.ts
+++ api/bot.ts
@@ -1,11 +1,24 @@
-import { createHandlers, createServer } from "@dressed/dressed/server";
-// @ts-ignore Should appear after bundling
-import { commandData, componentData, config } from "../dist/bot.gen.js";
+import { createHandlers, handleRequest } from "@dressed/dressed/server";
+// @ts-ignore Should appear after bundle
+import { commandData, componentData } from "../dist/bot.gen.js";
 
 const { runCommand, runComponent } = createHandlers(commandData, componentData);
-const app = createServer(runCommand, runComponent, config);
 
-export default async (req: Request, res: Response) => {
-    await app.ready();
-    return app.server.emit("request", req, res);
-};
+export async function POST(
+    req: Request,
+    res: { waitUntil: (p: unknown) => void },
+) {
+    return handleRequest(
+        {
+            headers: {
+                "x-signature-ed25519": req.headers.get("x-signature-ed25519"),
+                "x-signature-timestamp": req.headers.get(
+                    "x-signature-timestamp",
+                ),
+            },
+            text: await req.text(),
+        },
+        (i) => res.waitUntil(runCommand(i)),
+        (i) => res.waitUntil(runComponent(i)),
+    );
+}
